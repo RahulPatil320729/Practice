8d5d7e95db24133e434863b31d1c23b0817f6c24
Rahul Patil <rahul.patil@inceptiveconsulting.com>
2025-11-29 21:12:58 +0530

    Commit2 29-11-2025

--- src/main/java/com/practice/practice/stream/FindEvenNumber.java
+++ src/main/java/com/practice/practice/stream/FindEvenNumber.java
@@ -7,13 +7,17 @@ import java.util.stream.Collectors;
 public class FindEvenNumber {
 
 	public static void main(String[] args) {
-		
+
+		//List approach
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		
-		List<Integer> evenNumbers = numbers.stream().filter(n -> n%2==0).collect(Collectors.toList());
-		
-		System.out.println("Even Number : " +evenNumbers);
-		
+		List<Integer> evenNumbers = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+		System.out.println("Even Number in list : " + evenNumbers);
+
+		//Array approach
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int[] evenNumbers2 = Arrays.stream(a).filter(num -> num % 2 == 0).toArray();
+		System.out.println("Even Number in array : " +Arrays.toString(evenNumbers2));
+
 	}
 
 }
