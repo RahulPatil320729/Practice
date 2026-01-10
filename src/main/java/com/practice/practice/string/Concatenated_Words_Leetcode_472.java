3d1057c2c012696d4ade1dbfd414d8b1643189f7
Rahul Patil <rahul.patil@inceptiveconsulting.com>
2026-01-10 22:17:23 +0530

    isSubsequence_Leetcode_392 (Commit 10-01-2026)

--- src/main/java/com/practice/practice/string/Concatenated_Words_Leetcode_472.java
+++ src/main/java/com/practice/practice/string/Concatenated_Words_Leetcode_472.java
@@ -24,34 +24,34 @@ import java.util.Set;
 public class Concatenated_Words_Leetcode_472 {
 
 	public static void main(String[] args) {
-		String[] words = {"cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"};
+		String[] words = { "cat", "cats", "catsdogcats", "dog", "dogcatsdog", "hippopotamuses", "rat", "ratcatdogcat" };
 		System.out.println(findAllConcatenatedWordsInADict(words));
 	}
 
 	public static List<String> findAllConcatenatedWordsInADict(String[] words) {
 		HashMap<String, Boolean> hm = new HashMap<String, Boolean>();
 		Set<String> hs = new HashSet<String>();
-		for(String word:words) {
+		for (String word : words) {
 			hs.add(word);
 		}
-		List<String> result=new ArrayList<String>();
-		for(int i=0;i<words.length; i++) {
-			if(IsConcatenate(words[i], hs, hm)) {
+		List<String> result = new ArrayList<String>();
+		for (int i = 0; i < words.length; i++) {
+			if (IsConcatenate(words[i], hs, hm)) {
 				result.add(words[i]);
 			}
 		}
 		return result;
 	}
-	
+
 	public static boolean IsConcatenate(String word, Set<String> st, Map<String, Boolean> hm) {
-		if(hm.containsKey(word)) {
+		if (hm.containsKey(word)) {
 			return hm.get(word);
 		}
-		
-		for(int i=0;i<word.length();i++) {
-			String prefix=word.substring(0, i+1);
-			String suffix=word.substring(i+1, word.length());
-			if(st.contains(prefix)&& st.contains(suffix) || st.contains(prefix)&& IsConcatenate(suffix, st, hm)) {
+
+		for (int i = 0; i < word.length(); i++) {
+			String prefix = word.substring(0, i + 1);
+			String suffix = word.substring(i + 1, word.length());
+			if (st.contains(prefix) && st.contains(suffix) || st.contains(prefix) && IsConcatenate(suffix, st, hm)) {
 				hm.put(word, true);
 				return true;
 			}
