673c45abf999c919cedba4d37747440f41637e8d
Rahul Patil <rahul.patil@inceptiveconsulting.com>
2026-02-11 11:49:47 +0530

    Find_Words_Containing_Character_2942

---
+++ src/main/java/com/practice/practice/string/Find_Words_Containing_Character_2942.java
@@ -0,0 +1,43 @@
+package com.practice.practice.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//You are given a 0-indexed array of strings words and a character x.
+//Return an array of indices representing the words that contain the character x.
+//Note that the returned array may be in any order.
+
+//Example 1:
+//Input: words = ["leet","code"], x = "e"
+//Output: [0,1]
+//Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
+//Example 2:
+//Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+//Output: [0,2]
+//Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+
+//Example 3:
+//Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+//Output: []
+//Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+
+public class Find_Words_Containing_Character_2942 {
+
+	public static void main(String[] args) {
+		String words[] = {"abc","bcd","aaaa","cbc"};
+		char x = 'a';
+		System.out.println(findWordsContaining(words, x));
+	}
+
+	public static List<Integer> findWordsContaining(String[] words, char x) {
+		ArrayList<Integer> result = new ArrayList<>();
+		for(int i=0; i<words.length; i++) {
+			if(words[i].contains(String.valueOf(x))) {
+				result.add(i);
+			}
+		}
+		return result;
+	}
+
+}
