6960faf31d8d58a6abb080b4a3e67c4636de80da
rahulpatil_inceptive <rahul.patil@inceptiveconsulting.com>
2026-03-03 11:28:55 +0530

    Sum of the  digit in a number updated logic

--- src/main/java/com/practice/practice/rahulpatil/Sum_Of_Digits_In_A_Number.java
+++ src/main/java/com/practice/practice/rahulpatil/Sum_Of_Digits_In_A_Number.java
@@ -6,10 +6,15 @@ public class Sum_Of_Digits_In_A_Number {
 
 		// Number=1234
 		// Sum=10
+		
+		// Number=-78
+		// Sum=15
 
 		int number = 1234;
 		int sum = 0;
 
+		number = Math.abs(number);  // Ignore minus sign
+		
 		while (number > 0) {
 			sum = sum + number % 10;
 			number = number / 10;
